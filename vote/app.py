ce18bffacafed1d88777828145dcc8b26c7e90d2
shaikhaseena18 <shaik-haseena@hcltech.com>
2026-08-04 12:47:25 +0530

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -1,1 +1,13 @@
+from flask import Flask
+import redis
 
+app = Flask(__name__)
+r = redis.Redis(host='redis', port=6379)
+
+@app.route('/')
+def vote():
+    r.incr('cats')
+    return "Vote Recorded"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
